308e2d4c71fbd2094db0c4d1cb8cc87af7d611ac
chris-diesner <chr.diesner@mailbox.org>
2024-12-10 07:19:58 +0100

    final setup for frontend done & display basic board for testing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from '../pages/Home';
+import Home from './pages/Home';
+import Game from './pages/Game';
+import Board from './components/Board';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/board" element={<Board />} />
       </Routes>
     </Router>
   );
