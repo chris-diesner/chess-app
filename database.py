04e5c25763c66897d1e67d2f62e17dccd68c3a00
chris-diesner <chr.diesner@mailbox.org>
2025-02-04 12:10:15 +0100

    edit github actions workflow

--- database.py
+++ database.py
@@ -1,13 +1,21 @@
+import os
 from pymongo import MongoClient
 from dotenv import load_dotenv
-import os
 
+# .env laden (lokal), aber GitHub Actions nutzt die Secrets automatisch
 load_dotenv()
+
 MONGO_URI = os.getenv("MONGO_URI")
+
+if not MONGO_URI:
+    raise ValueError("❌ Fehler: MONGO_URI ist nicht gesetzt!")
+
 client = MongoClient(MONGO_URI)
-
 db = client["chess_db"]
-games_collection = db["games"]
-users_collection = db["users"]
 
-print("✅ Verbindung zur MongoDB Atlas hergestellt!")
+# Collections definieren
+users_collection = db["users"]
+games_collection = db["games"]
+moves_collection = db["moves_history"]
+
+print("✅ Verbindung zu MongoDB hergestellt!")
